129d3ed44e42ec98bcec3db9c35d9724a44bd0c7
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-06-01 15:51:45 -0300

    🔥 Update home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD:src/pages/home.tsx
-import { HeaderUser } from '../components/home/HeaderUser'
-import { Hero } from '../components/home/Hero'
-=======
 import { Hero } from "../components/home/Hero";
 import { HeaderUser } from "../components/home/HeaderUser";
 import { Title } from "../components/Title";
 import { Carousel } from "../components/Carousel";
 
->>>>>>> bf4e923 (\u{1F525} Criado carousel e titulo):src/pages/Home.tsx
 export function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 ">
