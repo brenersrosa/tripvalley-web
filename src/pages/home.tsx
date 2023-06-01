f6259e34f03f1f4dfc8d2a7c419c40ef19c4030a
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-06-01 03:29:16 -0300

    \u{1F525} Criado carousel e titulo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,25 @@
+<<<<<<< HEAD:src/pages/home.tsx
 import { HeaderUser } from '../components/home/HeaderUser'
 import { Hero } from '../components/home/Hero'
+=======
+import { Hero } from "../components/home/Hero";
+import { HeaderUser } from "../components/home/HeaderUser";
+import { Title } from "../components/Title";
+import { Carousel } from "../components/Carousel";
+
+>>>>>>> bf4e923 (\u{1F525} Criado carousel e titulo):src/pages/Home.tsx
 export function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 ">
       <HeaderUser></HeaderUser>
       <Hero></Hero>
+      <div>
+      <Title title="Categorias"></Title>
+      <Carousel></Carousel>
+      </div>
+      <div className="h-[200px]">
+
+      </div>
     </div>
-  )
+  );
 }
